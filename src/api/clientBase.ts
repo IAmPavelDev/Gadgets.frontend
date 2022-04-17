72a8c799081ff50f34ec8e13800d4bfb706127a5
Pave1_D3v <179werpavel@protonmail.com>
2022-04-17 23:18:43 +0300

    first

---
+++ src/api/clientBase.ts
@@ -0,0 +1,10 @@
+export class clientBase {
+	protected transformOptions(options: RequestInit) {
+		const token = localStorage.getItem("token");
+		options.headers = {
+			...options.headers,
+			Authorization: "Bearer " + token,
+		};
+		return Promise.resolve(options);
+	}
+}
